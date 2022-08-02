51f91c0bd083ea489d45f24afb62f5cf637066ff
Manav Sethi <msethi@yamaha-motor-india.com>
2022-08-02 21:53:43 +0530

    added route to get all drivers

--- src/routes/drivers.ts
+++ src/routes/drivers.ts
@@ -1,36 +1,39 @@
 import { Request, Response } from 'express';
+import MongoDB from '../db';
 
 import { Route, ServiceErrorResponse } from '../types/routing';
 import { Driver, GetDriverDetailsServiceResponse, GetDriversServiceResponse } from '../types/drivers';
+import { log } from 'console';
+import { EMPTY_STRING } from '../constants';
 
 export const driverRoutes: Route[] = [
   {
     method: 'GET',
     path: '/drivers',
-    callbackFunction: (request: Request, response: Response) => {
+    callbackFunction: async (request: Request, response: Response) => {
       try {
-        const drivers: Driver[] = [
-          {
-            _id: '1',
-            name: 'Driver 1',
-            phoneNumber: '1234567890'
-          },
-          {
-            _id: '2',
-            name: 'Driver 2',
-            phoneNumber: '1234567890'
-          },
-          {
-            _id: '3',
-            name: 'Driver 3',
-            phoneNumber: '1234567890'
-          },
-          {
-            _id: '4',
-            name: 'Driver 4',
-            phoneNumber: '1234567890'
-          }
-        ];
+        const MongoDBObject = new MongoDB();
+
+        if ((await MongoDBObject.db.collection('drivers').countDocuments()) === 0) {
+          const responseJson: ServiceErrorResponse = {
+            message: 'No records found',
+            description: 'No documents were found in the database.'
+          };
+          response.status(404).json(responseJson);
+          return;
+        }
+
+        const drivers: Driver[] = [];
+        const cursor = MongoDBObject.db.collection('drivers').find();
+        await cursor.forEach(function (document) {
+          drivers.push({
+            _id: document._id.toString(),
+            name: document.name,
+            phoneNumber: document.phoneNumber
+          });
+        });
+
+        MongoDBObject.close();
 
         const responseJson: GetDriversServiceResponse = {
           drivers
@@ -38,7 +41,7 @@ export const driverRoutes: Route[] = [
         response.status(200).json(responseJson);
       } catch (error) {
         const responseJson: ServiceErrorResponse = {
-          message: error.message,
+          message: error.class + ': ' + error.message,
           description: error.description
         };
         response.status(500).json(responseJson);
